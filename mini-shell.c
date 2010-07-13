ddf1aa82e242e69ebefdc717f52821aebb9f277b
luther <luther@luther.(none)>
2010-07-14 01:58:42 +0300

    Merge loopul de comenzi

--- mini-shell.c
+++ mini-shell.c
@@ -23,11 +23,13 @@ void run_command(word_t * comm, word_t * params)
 {
     if (params != NULL) {
         const char* para[] = {comm->string, params->string, NULL};
-        execvp(comm->string, (char *const *)para);
+        if ( fork() == 0)
+            execvp(comm->string, (char *const *)para);
     }
     else {
         const char* para[] = {comm->string, NULL};
-        execvp(comm->string, (char *const *)para);
+        if (fork() == 0)
+            execvp(comm->string, (char *const *)para);
     }
 }
 
@@ -36,31 +38,38 @@ int main(void)
 {
 	char line[1000];
 	command_t *root = NULL;
-
-	printf(PROMPT_STRING); fflush(stdout);
-	if (fgets(line, sizeof(line), stdin) == NULL)
-	{
-		fprintf(stderr, "End of file!\n");
-		return EXIT_SUCCESS;
-	}
-	/* we might have not read the entire line... */
-	if (parse_line(line, &root)) {
-		printf("Command successfully read!\n");
-		if (root->op == OP_NONE) {
-		    run_command(root->scmd->verb, root->scmd->params);
-		}
-		if (root == NULL) {
-			printf("Command is empty!\n");
-		}
-		else {
+   // freopen("cmd", "r", stdin);
+   printf(PROMPT_STRING); fflush(stdout);
+    while (1)
+    {
+        root = NULL;
+	    
+	    if (fgets(line, sizeof(line), stdin) == NULL)
+	    {
+		    fprintf(stderr, "End of file!\n");
+		    return EXIT_SUCCESS;
+	    }
+	    /* we might have not read the entire line... */
+	    if (parse_line(line, &root)) 
+	    {
+		    printf("Command successfully read!\n");
+		    if (root == NULL) {
+			    printf("Command is empty!\n");
+		    } else {
 			/* root points to a valid command tree that we can use */
+			if (strcmp(line, "quit") == 0)
+		        return EXIT_SUCCESS;
+		    
+		    if (root->op == OP_NONE)
+		        run_command(root->scmd->verb, root->scmd->params);
+		    }
 		}
-	}
-	else {
-		/* there was an error parsing the command */
-	}
-
-	free_parse_memory();
+	    else {
+		    /* there was an error parsing the command */
+	    }
 	
+    printf(PROMPT_STRING); fflush(stdout);
+	free_parse_memory();
+	}
 	return EXIT_SUCCESS;
 }
