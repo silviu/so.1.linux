546090dbdd3839079eca4ff11ac21e6d0e9ba4d3
luther <luther@luther.(none)>
2010-07-14 03:58:21 +0300

    Merge ls > aa

--- mini-shell.c
+++ mini-shell.c
@@ -2,6 +2,11 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#include <assert.h>
 
 #include "parser/parser.h"
 
@@ -17,19 +22,47 @@ void parse_error(const char * str, const int where)
 	fprintf(stderr, "Parse error near %d: %s\n", where, str);
 }
 
-
-
-void run_command(word_t * comm, word_t * params)
+int count_params(simple_command_t * s)
 {
-    if (params != NULL) {
-        const char* para[] = {comm->string, params->string, NULL};
-        if ( fork() == 0)
-            execvp(comm->string, (char *const *)para);
-    }
-    else {
-        const char* para[] = {comm->string, NULL};
-        if (fork() == 0)
-            execvp(comm->string, (char *const *)para);
+    word_t * crt = s->params;
+    int i = 0;
+    while (crt != NULL) {
+        i++;
+		crt = crt->next_word;
+	}
+	return i;
+}
+
+char** get_params(simple_command_t * s)
+{
+    word_t * crt = s->params;
+    int param_no = count_params(s);
+    char** param_vector = malloc (sizeof(char*) * (param_no + 2));
+
+    param_vector[0] = s->verb->string;
+    int i = 1;
+    while (crt != NULL) {
+        param_vector[i] = crt->string;
+		crt = crt->next_word;
+		i++;
+	}
+	param_vector[i] = NULL;
+	return param_vector;
+}
+
+void run_command(simple_command_t * s)
+{
+    char** pp = get_params(s);
+    if ( fork() == 0)
+    {
+        if (s->out != NULL)
+        {
+            int fd = open(s->out->string, O_CREAT|O_RDWR|O_TRUNC, 0644);
+            if (-1 == fd)
+                printf("Could not open file.");
+                dup2(fd, 1);
+        }
+        execvp(s->verb->string, (char *const *)pp);
     }
 }
 
@@ -56,13 +89,32 @@ int main(void)
 		    if (root == NULL) {
 			    printf("Command is empty!\n");
 		    } else {
+		    
+		    
 			/* root points to a valid command tree that we can use */
 			if (strcmp(line, "quit") == 0)
 		        return EXIT_SUCCESS;
 		    
-		    if (root->op == OP_NONE)
-		        run_command(root->scmd->verb, root->scmd->params);
+		    if (root->op == OP_NONE) {
+		            run_command(root->scmd);
 		    }
+		     else { 
+		            switch (root->op) {
+		                case OP_SEQUENTIAL:
+			                break;
+		                case OP_PARALLEL:
+			                break;
+		                case OP_CONDITIONAL_ZERO:
+			                break;
+		                case OP_CONDITIONAL_NZERO:
+			                break;
+		                case OP_PIPE:
+			                break;
+		                default:
+			                assert(false);
+		            }
+		     }
+		}
 		}
 	    else {
 		    /* there was an error parsing the command */
